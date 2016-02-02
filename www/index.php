05be7c6854f3f56cc89087e4470b3151a2270e26
Dan Clarke <OverByThere@users.noreply.github.com>
2016-02-02 17:48:42 +0000

    Started work on basic HTML page to preview the data.

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>checkPlanning</title>
+      <style type="text/css">
+         body { font-family: sans-serif; }
+      </style>
+      <script type="text/javascript" src="/inc/jquery-1.12.0.min.js"></script>
+      <script type="text/javascript">
+         $(document).ready(function() {
+            
+         });
+      </script>
+   </head>
+   <body>
+
+   </body>
+</html>
