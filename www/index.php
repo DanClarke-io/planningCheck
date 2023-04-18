bbbed0534e479261c8e55b07e87fb83eefc918fd
Dan Clarke <OverByThere@users.noreply.github.com>
2023-04-18 13:48:33 +0100

    Upgrading jQuery due to security fix (2/3)

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@
       <style type="text/css">
          body { font-family: sans-serif; }
       </style>
-      <script type="text/javascript" src="/inc/jquery-1.12.0.min.js"></script>
+      <script type="text/javascript" src="/inc/jquery-3.6.4.min.js"></script>
       <script type="text/javascript">
          $(document).ready(function() {
 
